2fd671879f5703456bcb23efc81584d252ef109a
Vimal Vashisth <vimalvas03@gmail.com>
2020-11-27 14:05:44 +0530

    DOne

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
+const connectDb = require('./config/db');
 
 const app = express();
+
+connectDb();
 
 
 //Setting up dotenv
 dotenv.config({path: './config/config.env'});
 
-//Path to routes
-
-
-
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/bootcamps', bootcamps);
+
+
+process.on('unhandledRejection', (err, promise)=>{
+	console.log('Unhandled Error : ' , err.message);
+
+	//close the server
+	server.close(()=>process.exit(1));
+});
 
 
 app.listen(PORT, ()=>{
